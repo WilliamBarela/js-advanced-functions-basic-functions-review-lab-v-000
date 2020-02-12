53aabff467281d8a845ccf47e9882157ff3e2bc3
William Barela <william.barela.dev@gmail.com>
2020-02-12 18:35:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,3 +26,11 @@ function(){console.log("do something")}
 () => {console.log("do something")
 */
 
+//closures ES6
+
+const wrapAdjective = (flair = "*") => {
+  return (quality = "special") => {
+    return `You are ${flair + quality + flair}.`
+  }
+}
+

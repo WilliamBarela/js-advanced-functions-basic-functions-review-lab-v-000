1d71dee06b5946c7378ac263939b0074aad3db86
William Barela <william.barela.dev@gmail.com>
2020-02-12 17:54:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Your code here
 
-// ES5
+// ES5, function declaration. By default, it may be hoisted
 /*
 function saturdayFun(activity="roller-skate"){
   return `This Saturday, I want to ${activity}!`
@@ -12,6 +12,14 @@ const saturdayFun = (activity = "roller-skate") => {
   return `This Saturday, I want to ${activity}!`
 }
 
+// both of these two are Functional expressions (not hoisted)
 const mondayWork = (activity = "go to the office") => {
   return  `This Monday, I will ${activity}.`
 }
+
+//Anonymous Functions:
+// ES5
+
+/*
+
+*/

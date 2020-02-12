f3685bb6fb8015464a648c0504038ec0d18df0f6
William Barela <william.barela.dev@gmail.com>
2020-02-12 17:47:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,8 +8,10 @@ function saturdayFun(activity="roller-skate"){
 */
 
 // ES6
-const saturdayFun = 
-(activity = "roller-skate") => {
+const saturdayFun = (activity = "roller-skate") => {
   return `This Saturday, I want to ${activity}!`
-  
+}
+
+const mondayWork = (activity = "go to the office") => {
+  return  `This Monday I will ${activity}.`
 }
